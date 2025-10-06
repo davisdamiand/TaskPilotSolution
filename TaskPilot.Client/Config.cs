0de7b8ea3ed24e4fe710d1e419cfeef6c45fc0d2
davisdamiand <davisdamiand@outlook.com>
2025-10-06 16:09:32 +0200

    Authentication completed Local store of ID

---
+++ TaskPilot.Client/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPilot.Client
+{
+    public class Config
+    {
+        public const string BaseUrl = "https://localhost:7192/";
+    }
+}
--- TaskPilot.Server/Program.cs
+++ TaskPilot.Server/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TaskPilot.Server.Data;
+using TaskPilot.Server.Interfaces;
+using TaskPilot.Server.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 //Register context
 builder.Services.AddDbContext<TaskPilotContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IStudentService, StudentService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
