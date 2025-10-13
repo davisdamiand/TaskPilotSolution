e955598d58a7f639310f770d59d6fe74eebf07f4
LesegoRM <lesegoramaruta99@gmail.com>
2025-10-13 20:11:20 +0200

    Added login.xaml

--- TaskPilot.Client/LoginPage.xaml.cs
+++ TaskPilot.Client/LoginPage.xaml.cs
@@ -6,15 +6,24 @@ namespace TaskPilot.Client;
 public partial class LoginPage : ContentPage
 {
     private readonly HttpClient _httpClient;
+
+    // Add a reference to the NameLabel control
+    private Label NameLabel;
+
     public LoginPage()
 	{
-		InitializeComponent();
+        // Ensure the correct namespace is used for InitializeComponent
+        // If this is a .NET MAUI or Xamarin.Forms project, this method is auto-generated
+        // and should be available in the partial class.
+        this.InitializeComponent(); // Add 'this.' to call the method from the partial class
 
         _httpClient = new HttpClient
         {
-            //Stored in a common config class
             BaseAddress = new Uri(Config.BaseUrl)
         };
+
+        // Find the NameLabel control from the XAML by its name
+        NameLabel = this.FindByName<Label>("NameLabel");
     }
 
     private async void RunOnClick(object sender, EventArgs e)
