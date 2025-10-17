a01676bcdb10906ef37dad06a1041f6893d61718
LesegoRM <lesegoramaruta99@gmail.com>
2025-10-17 19:47:51 +0200

    made some changes to the todo page and provided code for the forgot password page and some of the stats page but i was just playing around withe the stats page

--- TaskPilot.Client/ForgotPasswordPage.xaml.cs
+++ TaskPilot.Client/ForgotPasswordPage.xaml.cs
@@ -1,9 +1,48 @@
-namespace TaskPilot.Client;
+using Microsoft.Maui.Controls;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-public partial class ForgotPasswordPage : ContentPage
+namespace TaskPilot.Client
 {
-	public ForgotPasswordPage()
-	{
-		InitializeComponent();
-	}
+    public partial class ForgotPasswordPage : ContentPage
+    {
+        public ForgotPasswordPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnResetClicked(object sender, EventArgs e)
+        {
+            string email = EntryResetEmail.Text?.Trim();
+
+            // Validate email
+            if (string.IsNullOrEmpty(email))
+            {
+                EmailErrorLabel.Text = "Please enter your email address.";
+                EmailErrorLabel.IsVisible = true;
+                return;
+            }
+
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                EmailErrorLabel.Text = "Please enter a valid email address.";
+                EmailErrorLabel.IsVisible = true;
+                return;
+            }
+
+            EmailErrorLabel.IsVisible = false;
+
+            // Simulate sending email
+            await Task.Delay(1500);
+
+            MessageLabel.Text = "A password reset link has been sent to your email.";
+            MessageLabel.IsVisible = true;
+        }
+
+        private async void OnBackToLoginClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+    }
 }
--- TaskPilot.Client/StatsPage.xaml.cs
+++ TaskPilot.Client/StatsPage.xaml.cs
@@ -1,9 +1,38 @@
-namespace TaskPilot.Client;
+using Microsoft.Maui.Controls;
+using System;
 
-public partial class StatsPage : ContentPage
+namespace TaskPilot.Client
 {
-	public StatsPage()
-	{
-		InitializeComponent();
-	}
+    public partial class StatsPage : ContentPage
+    {
+        public StatsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCourseraClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://www.coursera.org/");
+        }
+
+        private async void OnUdemyClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://www.udemy.com/");
+        }
+
+        private async void OnFreeCodeCampClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://www.freecodecamp.org/");
+        }
+
+        private async void OnKhanAcademyClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://www.khanacademy.org/");
+        }
+
+        private async void OnLinkedInLearningClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://www.linkedin.com/learning/");
+        }
+    }
 }
